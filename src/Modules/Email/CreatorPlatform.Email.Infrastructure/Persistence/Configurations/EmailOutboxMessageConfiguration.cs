86a151dd7ec7dc0c9c0e8717ac14c0c7f12c1fbb
franogugic <franogugic@icloud.com>
2026-05-21 21:13:53 +0200

    feat: add email outbox persistence

---
+++ src/Modules/Email/CreatorPlatform.Email.Infrastructure/Persistence/Configurations/EmailOutboxMessageConfiguration.cs
@@ -0,0 +1,48 @@
+using CreatorPlatform.Email.Domain.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CreatorPlatform.Email.Infrastructure.Persistence.Configurations;
+
+public sealed class EmailOutboxMessageConfiguration : IEntityTypeConfiguration<EmailOutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<EmailOutboxMessage> builder)
+    {
+        builder.ToTable("email_outbox_messages", "email");
+
+        builder.HasKey(message => message.Id);
+
+        builder.Property(message => message.ToEmail)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(message => message.Subject)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(message => message.HtmlBody)
+            .IsRequired();
+
+        builder.Property(message => message.PlainTextBody)
+            .IsRequired();
+
+        builder.Property(message => message.Status)
+            .HasConversion<string>()
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(message => message.RetryCount)
+            .IsRequired();
+
+        builder.Property(message => message.NextAttemptAt)
+            .IsRequired();
+
+        builder.Property(message => message.LastError)
+            .HasMaxLength(1000);
+
+        builder.Property(message => message.CreatedAt)
+            .IsRequired();
+
+        builder.HasIndex(message => new { message.Status, message.NextAttemptAt });
+    }
+}
